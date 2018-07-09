f2b5ae74022aa500bfd23792c76317e2fecd475c
reddevv <40369620+reddevv@users.noreply.github.com>
2018-07-09 07:49:05 +0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, request, abort
+
+app = Flask(__name__)
+
+@app.route("/callback")
+def kkk()
+    return("lol")
+    
+import os
+if __name__ == "__main__":
+    app.run()    
